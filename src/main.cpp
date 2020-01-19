41cd9b30d8f5f78d5c76b705af77461e66ca05af
VertexF <danmanley93@gmail.com>
2020-01-19 16:12:50 +0000

    Linked in OpenGL and updated the git ignore to exclude resources.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
-//#include "Game.h"
-#include "SDL.h"
-#include "GL/glew.h"
-#include "il/il.h"
+#include "Game.h"
 /**
     Update the player so the player has a camera.
 */
 int main(int argv, char** argc)
 {
-    //Game game;
-    //game.mainLoop();
-
-    SDL_Init(SDL_INIT_EVERYTHING);
-    glewInit();
-    ilInit();
+    Game game;
+    game.mainLoop();
 
     return 0;
 }
